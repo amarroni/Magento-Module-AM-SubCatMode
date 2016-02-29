7246725b8bd91b244b8c4cca44b4d32a2d876aa3
Oscar Recio <osrecio@gmail.com>
2016-02-29 15:09:10 +0100

    getAllOptions from parent
    getAllOptions from parent, the purpose is that the parent changes, or added new modes in future your module get new modes of categories

--- code/local/AM/SubCatMode/Model/Catalog/Category/Attribute/Source/Mode.php
+++ code/local/AM/SubCatMode/Model/Catalog/Category/Attribute/Source/Mode.php
@@ -2,28 +2,16 @@
 
 class AM_SubCatMode_Model_Catalog_Category_Attribute_Source_Mode extends Mage_Catalog_Model_Category_Attribute_Source_Mode
 {
+    
     public function getAllOptions()
     {
-        if (!$this->_options) {
-            $this->_options = array(
-                array(
-                    'value' => Mage_Catalog_Model_Category::DM_PRODUCT,
-                    'label' => Mage::helper('catalog')->__('Products only'),
-                ),
-                array(
-                    'value' => Mage_Catalog_Model_Category::DM_PAGE,
-                    'label' => Mage::helper('catalog')->__('Static block only'),
-                ),
-                array(
-                    'value' => Mage_Catalog_Model_Category::DM_MIXED,
-                    'label' => Mage::helper('catalog')->__('Static block and products'),
-                ),
-                array(
+        $listModes = parent::getAllOptions();
+        
+        $newlistmodes[] = array(
                     'value' => AM_SubCatMode_Model_Catalog_Category::DM_SUBCATEGORIES,
-                    'label' => Mage::helper('catalog')->__('Subcategories only'),
-                )
-            );
-        }
-        return $this->_options;
+                    'label' => Mage::helper('catalog')->__('Subcategories only')
+                    );
+
+        return array_merge($listModes,$newlistmodes);;
     }
 }
